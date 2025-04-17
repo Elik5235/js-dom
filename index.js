8a8b0de22611e1a34a16ba93288b032209527bf9
Elik <eliseyignatchenko06@gmail.com>
2025-04-17 12:58:29 +0300

    getElementsByClassName, getElementsByTagName

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
+const collection = document.getElementsByClassName('paragraph');
 
-const form = document.getElementById('name-form');
+// for(let i = 0; i < collection.length; i++) {
+//     collection[i].style.color = 'green';
+// }
 
-form.addEventListener('submit', consoleForm);
+// for ... of - спеціальний цикл, який перебирає об'єкт по ключам
 
-function consoleForm(event) {
-    event.preventDefault();
 
-    const form = event.target;
-    const value = form[0].value;
-    console.log(value);
+for(let p of collection) {
+    p.style.color = 'green';
+}
+
+const btns = document.getElementsByTagName('button');
+const arr = [...btns];
+
+arr.forEach((button) => {
+    button.style.color = 'red';
+});
+
+function hello(event) {
+    console.log('Hello user');
+
+    event.target.removeEventListener('click', hello);
+}
+
+for(let btn of btns) {
+    btn.addEventListener('click', hello);
 }
