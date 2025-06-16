810ceb4ffbd725742f41704dcf5ed54767245b2e
Elik <eliseyignatchenko06@gmail.com>
2025-06-16 13:21:59 +0300

    Option and stopPropagation

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
-const CURRENCY = {
-  USD_TO_UAH: 37.37,
-  EUR_TO_UAH: 40.0,
-};
+const button = document.querySelector('button');
+const div = document.querySelector('#root')
 
-const converterForm = document.querySelector("#currency-converter-form");
-
-converterForm.addEventListener('submit', converterHandler);
-
-function converterHandler(event) {
-  event.preventDefault();
-  const amount = Number(document.querySelector('#amount').value);
-  const currency = document.querySelector('#currency').value;
-
-  let convertedAmount;
-  if (currency === "USD") {
-    convertedAmount = amount * CURRENCY.USD_TO_UAH;
-  } else if (currency === "EUR") {
-    convertedAmount = amount * CURRENCY.EUR_TO_UAH;
-  }
-  const resultDiv = document.querySelector('#result');
-  resultDiv.innerHTML = `${amount} ${currency} = ${convertedAmount} UAH`;
+function eventHandlerButton(event){
+  console.log('Hi button');
 }
+
+ function eventHandlerBody(event){
+  console.log('HI body')
+ }
+ button.addEventListener('click',eventHandlerButton,{capture: true});
+//  div.addEventListener('click',eventHandler,{capture: true});
+ document.body.addEventListener('click', eventHandlerBody,{capture: true});
+//  window.addEventListener('click', eventHandler,{capture: true});
