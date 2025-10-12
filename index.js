2d7937bd45dad4430211535a0be692a33d80531d
Elik <eliseyignatchenko06@gmail.com>
2025-10-13 02:53:01 +0300

    practise task resovle while-do while

--- index.js
+++ index.js
@@ -1,14 +1,50 @@
-const button = document.querySelector('button');
-const div = document.querySelector('#root')
+// function bigNum(num1, num2) {
+//   if (num1 > num2) {
+//     console.log(num1);
+//   } else {
+//     console.log(num2);
+//   }
+// }
+// bigNum(10, 20);
 
-function eventHandlerButton(event){
-  console.log('Hi button');
+// function podil(num) {
+//   if (num % 2 === 0) {
+//     console.log("Число ділиться на два");
+//   } else {
+//     console.log("число не ділиться на два");
+//   }
+// }
+// podil(3);
+
+// let it = 0;
+
+// while (it < 10) {
+//   console.log(1);
+//   it++;
+// }
+
+// while(true){
+//   console.log('НЄ');
+//   break 
+// }
+
+// let i = 0;
+// while(i < 10){
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   } 
+//   i++;
+// }
+
+// let i = 0;
+// while(true){
+//   i++
+// }
+
+
+let userPassword = prompt("please enter true password")
+let truePassword = 'qwerty123';
+
+while(userPassword !== truePassword){
+  userPassword = prompt("password is not true, please again")
 }
-
- function eventHandlerBody(event){
-  console.log('HI body')
- }
- button.addEventListener('click',eventHandlerButton,{capture: true});
-//  div.addEventListener('click',eventHandler,{capture: true});
- document.body.addEventListener('click', eventHandlerBody,{capture: true});
-//  window.addEventListener('click', eventHandler,{capture: true});
