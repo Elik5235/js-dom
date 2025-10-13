5785329e86ec28e40779f1af124c079e64c994f7
Elik <eliseyignatchenko06@gmail.com>
2025-10-13 03:23:02 +0300

    factorial practise task resolve

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-for(let i = 0; i < 10; i++){
-  console.log(i);
+// for(let i = 0; i < 10; i++){
+//   console.log(i);
+//
+
+function factorial(num) {
+  let result = 1;
+  for (let i = 1; i <= num; i++) {
+    if (num === 0 || num === 1) {
+      return 1;
+    } else if (num < 0) {
+      return null;
+    } else {
+      result = result * i;
+    }
+  }
+  return result;
 }
+console.log(factorial(3));
