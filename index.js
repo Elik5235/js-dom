2b9b6125b9764fb7a2f35ab1b4643a6b4b874d7a
Elik <eliseyignatchenko06@gmail.com>
2025-10-16 11:19:36 +0300

    object

--- index.js
+++ index.js
@@ -2,17 +2,51 @@
 //   console.log(i);
 //
 
-function factorial(num) {
-  let result = 1;
-  for (let i = 1; i <= num; i++) {
-    if (num === 0 || num === 1) {
-      return 1;
-    } else if (num < 0) {
-      return null;
-    } else {
-      result = result * i;
-    }
-  }
-  return result;
+// function factorial(num) {
+//   let result = 1;
+//   for (let i = 1; i <= num; i++) {
+//     if (num === 0 || num === 1) {
+//       return 1;
+//     } else if (num < 0) {
+//       return null;
+//     } else {
+//       result = result * i;
+//     }
+//   }
+//   return result;
+// }
+// console.log(factorial(3));
+
+// function dilena(num) {
+//   if (num % 5 === 0) {
+//     console.log("число ділиться на 5");
+//   } else {
+//     console.log("число не ділиться на 5");
+//   }
+// }
+// console.log(dilena(11));
+
+// function diapason(start, end){
+//   for(let i = start; i <= end; i++){
+//     if(i % 5 === 0){
+//       console.log("числа діляться на 5");
+//     }
+//   } 
+// }
+// diapason(10,50);
+
+
+// const obj = {
+// color:'white',
+// fontSize: 2,
+
+// }
+
+const cat = {
+  name: 'murz',
+  color:'red',
+  breed:'dvor',
+  age:1,
+
 }
-console.log(factorial(3));
+cat.age = cat.age + 1
