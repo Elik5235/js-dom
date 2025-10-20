355141f5c4e7430e9a8c26222caf6b72b726478f
Elik <eliseyignatchenko06@gmail.com>
2025-10-20 11:08:23 +0300

    function-constructor

--- index.js
+++ index.js
@@ -1,52 +1,8 @@
-// for(let i = 0; i < 10; i++){
-//   console.log(i);
-//
-
-// function factorial(num) {
-//   let result = 1;
-//   for (let i = 1; i <= num; i++) {
-//     if (num === 0 || num === 1) {
-//       return 1;
-//     } else if (num < 0) {
-//       return null;
-//     } else {
-//       result = result * i;
-//     }
-//   }
-//   return result;
-// }
-// console.log(factorial(3));
-
-// function dilena(num) {
-//   if (num % 5 === 0) {
-//     console.log("число ділиться на 5");
-//   } else {
-//     console.log("число не ділиться на 5");
-//   }
-// }
-// console.log(dilena(11));
-
-// function diapason(start, end){
-//   for(let i = start; i <= end; i++){
-//     if(i % 5 === 0){
-//       console.log("числа діляться на 5");
-//     }
-//   } 
-// }
-// diapason(10,50);
-
-
-// const obj = {
-// color:'white',
-// fontSize: 2,
-
-// }
-
-const cat = {
-  name: 'murz',
-  color:'red',
-  breed:'dvor',
-  age:1,
-
+function Cat(name,color,age,breed){
+  this.name = name;
+  this.color = color;
+  this.age = age;
+  this.breed = breed;
 }
-cat.age = cat.age + 1
+
+const cat1 = new Cat('bublik','red','siam',4)
